34760d57a948abad4494ce453c71549a08e9d479
Rose <rose@Desktop.lan>
2026-05-24 12:34:48 +0100

    Initial commit

---
+++ Drawers/Plugin.cs
@@ -0,0 +1,91 @@
+﻿using Dalamud.Game.ClientState.Objects.Enums;
+using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Client.System.String;
+using FFXIVClientStructs.FFXIV.Client.UI;
+
+namespace Drawers;
+
+public sealed unsafe class Plugin : IDalamudPlugin
+{
+
+    private const string CommandName = "/drawers";
+
+    [PluginService]
+    internal static ICommandManager CommandManager { get; private set; } = null!;
+
+    [PluginService]
+    internal static IClientState ClientState { get; private set; } = null!;
+
+    [PluginService]
+    internal static IObjectTable ObjectTable { get; private set; } = null!;
+
+    [PluginService]
+    internal static IFramework Framework { get; private set; } = null!;
+
+    [PluginService]
+    internal static IPluginLog Log { get; private set; } = null!;
+
+    public Plugin()
+    {
+        CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+        {
+            HelpMessage = "Toggle weapon draw/sheathe"
+        });
+
+        Log.Information("Drawers loaded");
+    }
+
+    public void Dispose()
+    {
+        CommandManager.RemoveHandler(CommandName);
+    }
+
+    private void OnCommand(string command, string args)
+    {
+        if (!ClientState.IsLoggedIn)
+            return;
+
+        var player = ObjectTable.LocalPlayer;
+        if (player == null)
+            return;
+        
+        // Check if weapon is out
+        bool wasWeaponOut = player.StatusFlags.HasFlag(StatusFlags.WeaponOut);
+        Log.Information($"Initial weapon state: {wasWeaponOut}");
+
+        // Emote
+        ExecuteGameCommand(wasWeaponOut ? "/sheathe motion" : "/draw motion");
+       
+        Framework.RunOnTick(() =>
+        {
+            // If it worked
+            var currentPlayer = ObjectTable.LocalPlayer;
+            if (currentPlayer == null)
+                return;
+
+            bool isWeaponOut = currentPlayer.StatusFlags.HasFlag(StatusFlags.WeaponOut);
+
+            Log.Information($"Weapon state after emote: {isWeaponOut}");
+
+            // If it didn't work
+            if (isWeaponOut == wasWeaponOut)
+            {
+                Log.Warning("Emote failed, using /bm fallback");
+
+                ExecuteGameCommand("/bm");
+            }
+
+        }, delayTicks: 30);
+    }
+
+    private static void ExecuteGameCommand(string command)
+    {
+        using var utf8 = new Utf8String();
+        utf8.SetString(command);
+
+        UIModule.Instance()->ProcessChatBoxEntry(&utf8);
+    }
+}
